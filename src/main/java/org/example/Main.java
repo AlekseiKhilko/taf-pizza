92f37ce1167b4ec81dc6f5f01541bbbd6a666843
trainingby1 <trainingby1@google.com>
2022-11-30 22:36:53 +0300

    clean up

--- src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-        //System.out.println("Hello world!");
-        Pizza pizza = new Pizza();
-        //pizza.openMainPage();
-    }
-}
